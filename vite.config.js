4edb1e16bbe6e4a320dc4ed5bfcdfe63ec24ec80
joelOfTheSharingan <joelmgeorge.2010@icloud.com>
2025-11-06 19:37:27 +0530

    Added new files

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',     // Ensures relative paths for GitHub Pages
+  build: {
+    outDir: 'docs',  // So build goes directly into docs/
+  },
+})
+
